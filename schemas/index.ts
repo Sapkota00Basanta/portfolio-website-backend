af2f6e652b62b019202b67b7983f426f620057b7
Basanta Sapkota <sapkota.basanta46@gmail.com>
2023-01-01 20:30:40 +0545

    Added Schemas required for portfolio content

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,21 @@
 // Import schemas from other modules
-import testimonial from "./portfolio/testimonial"
+import about from './portfolio/about'
+import brand from './portfolio/brand'
+import contact from './portfolio/contact'
+import experience from './portfolio/experience'
+import skill from './portfolio/skill'
+import work from './portfolio/work'
+import workExperience from './portfolio/workExperience'
+import testimonial from './portfolio/testimonial'
 
+// Define Schema in this array
 export const schemaTypes = [
-    testimonial
+  about,
+  contact,
+  brand,
+  skill,
+  work,
+  experience,
+  workExperience,
+  testimonial,
 ]
